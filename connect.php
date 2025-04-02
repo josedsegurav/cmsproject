cc86838b7ffd028fe7d0dee84cb31890832daee0
Jose Segura <josed.segurav@gmail.com>
2025-04-01 22:01:14 -0500

    fixed variables test deploy

--- connect.php
+++ connect.php
@@ -18,9 +18,9 @@
     $database = ltrim($dbparts['path'],'/');
 
     try {
-        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+        $db = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
         // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully";
         }
     catch(PDOException $e)
